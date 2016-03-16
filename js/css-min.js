9f40086fcd1936118693bb10d4327195a0c98ba7
markof <markof@BigMac.local>
2016-03-16 23:39:47 +0800

    修正上线问题

--- js/css-min.js
+++ js/css-min.js
@@ -1,1 +1,1 @@
-function Css(){this.get=function(o,r){console.log(o),$.ajax({url:"http://127.0.0.1:8800/css/",dataType:"json",data:{css:o},success:function(o){"false"===o.error?r(null,o.data):r(new Error(o.errorcode))},error:function(o){r(o)}})}}
+function Css(){this.get=function(r,s){$.ajax({url:"http://markof.cn:8800/css/",dataType:"json",data:{css:r},success:function(r){"false"===r.error?s(null,r.data):s(new Error(r.errorcode))},error:s(err)})}}
